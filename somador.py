438220352352655aedae9efb67ac1577968ac5ea
Giancsa <gian.69889868@gmail.com>
2026-07-10 21:48:49 -0300

    Create somador.py

---
+++ somador.py
@@ -0,0 +1,9 @@
+# Recebe um número do usuário e soma com outro
+
+outro_numero = 5
+
+num_usuario = int(input("Digite um número inteiro: "))
+
+print(type(num_usuario))
+
+print(outro_numero + num_usuario)
