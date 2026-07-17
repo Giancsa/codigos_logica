1532d1ef94e400eaf9659c31355fa2c406e579c0
Giancsa <gian.69889868@gmail.com>
2026-07-17 10:11:04 -0300

    Exclusão

--- Exercicios 15.07.2026/e02.py
+++
@@ -1,9 +0,0 @@
-texto = ("Olá mundo")
-num_inteiro = 32
-num_decimal = 3.2
-booleano = True
-
-print(type(texto))
-print(type(num_inteiro))
-print(type(num_decimal))
-print(type(booleano))
--- Exercicios 15.07.2026/e03.py
+++
@@ -1,8 +0,0 @@
-frutas = ["banana", "maçã", "morango", "abacaxi", "melancia"]
-print(frutas)
-print(frutas[0])
-print(frutas[4])
-frutas.append("pessêgo")
-frutas.append("melão")
-print("A lista contém até o momento", frutas)
-print("O ultimo item da lista é:", frutas[-1])
