50b43ba958b7b0c299160d7e42f9f94e13a934ab
Giancsa <gian.69889868@gmail.com>
2026-07-17 10:09:39 -0300

    Exercicios
    Exercícios de lógica

---
+++ Exercicios 15.07.2026/e02.py
@@ -0,0 +1,23 @@
+"""Questão 2 – Identificando tipos de dados 
+
+Crie variáveis contendo:  
+Um texto 
+Um número inteiro 
+Um número decimal 
+Um valor booleano 
+Depois, utilize a função type() para exibir o tipo de cada variável. 
+Exemplo de saída: 
+<class 'str'> 
+<class 'int'> 
+<class 'float'> 
+<class 'bool'>""" 
+
+texto = ("Olá mundo")
+num_inteiro = 32
+num_decimal = 3.2
+booleano = True
+
+print(type(texto))
+print(type(num_inteiro))
+print(type(num_decimal))
+print(type(booleano))
---
+++ Exercicios 15.07.2026/e03.py
@@ -0,0 +1,17 @@
+"""Questão 3 – Lista de frutas 
+
+Crie uma lista contendo pelo menos 5 frutas. 
+Depois: 
+Exiba a lista completa. 
+Exiba apenas a primeira fruta. 
+Exiba apenas a última fruta. 
+Adicione algumas frutas e exiba a última, independentemente da quantidade. """
+
+frutas = ["banana", "maçã", "morango", "abacaxi", "melancia"]
+print(frutas)
+print(frutas[0])
+print(frutas[4])
+frutas.append("pessêgo")
+frutas.append("melão")
+print("A lista contém até o momento", frutas)
+print("O ultimo item da lista é:", frutas[-1])
