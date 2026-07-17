c9c24f03480aad09a7af8fa2fdaa78c84abe057d
Giancsa <gian.69889868@gmail.com>
2026-07-17 10:32:56 -0300

    Create e05.py

---
+++ Exercicios 15.07.2026/e05.py
@@ -0,0 +1,20 @@
+"""Questão 5 – Sistema de notas: 
+
+Crie uma variável chamada nota. 
+Utilize if...elif...else para classificar: 
+Nota - Resultado 
+9 a 10 - Excelente 
+7 a 8.9 - Aprovado 
+5 a 6.9 - Recuperação 
+Menor que 5 - Reprovado """
+
+nota = float(input("Digite a nota do aluno: "))
+while nota >10:
+    nota = float(input("Nota maior que a permitida, digite novamente "))
+if nota >=9:
+    print("Excelente")
+elif nota >=7:
+    print("Aprovado")
+else:
+    print("Rcuperação")
+
