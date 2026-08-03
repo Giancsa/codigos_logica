fa17eeec96a2d45f09a1bccc7436d4e9d6bd1e46
Giancsa <gian.69889868@gmail.com>
2026-08-03 18:39:09 -0300

    Update e13.py

--- Exercicios 15.07.2026/e13.py
+++ Exercicios 15.07.2026/e13.py
@@ -2,6 +2,4 @@ numero = int(input("Digite o numero desejado para tabuada: "))
 num2 = int(input("Digite até quanto a tabuada irá: "))
 
 for x in range (num2 + 1): 
-    #print(f"{x} x {numero} = {x*numero}")
-    print(x*numero)
-    
+    print(f"{x} x {numero} = {x*numero}")    
