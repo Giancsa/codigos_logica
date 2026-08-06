bb1cc2b0e0a2a4117a448f597b2d345c3c8e7481
Giancsa <gian.69889868@gmail.com>
2026-08-06 09:42:30 -0300

    Update e14.py

--- Exercicios 15.07.2026/e14.py
+++ Exercicios 15.07.2026/e14.py
@@ -3,10 +3,15 @@ import re
 regex_cpf = r"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$"
 
 while True:
-    cpf = input("Digite seu cpf para cadastro: ")
-    if re.fullmatch(regex_cpf, cpf):
-        print("CPF válido")
-        break
-    else:
-        print("CPF Inválido, digite novamente: ")
-        print("="*30)
+    print("Opção 1 = Cadastrar CPF")
+    print("Opcção 2 = Consultar CPF")
+
+    opcao = int(input("Digite uma opçao"))
+    if opcao == 1:
+        cpf = input("Digite seu cpf para cadastro: ")
+        if re.fullmatch(regex_cpf, cpf):
+                print("CPF válido")
+                break
+        else:
+                print("CPF Inválido, digite novamente: ")
+                print("="*30)
