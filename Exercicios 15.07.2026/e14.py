6d9c08737de8c85aeba503b8d6628bd965959c41
Giancsa <gian.69889868@gmail.com>
2026-08-08 16:55:23 -0300

    Update e14.py

--- Exercicios 15.07.2026/e14.py
+++ Exercicios 15.07.2026/e14.py
@@ -1,17 +1,24 @@
-#Criar um cadastro a partir do cpf
+'''Criar um cadastro a partir do cpf recebendo dados do usuário (CPF, NOME, NASCIMENTO, EMAIL, TELEFONE)
+e consultar dos dados no dicionário através do e-mail'''
 import re
 regex_cpf = r"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$"
 
 while True:
-    print("Opção 1 = Cadastrar CPF")
-    print("Opcção 2 = Consultar CPF")
 
-    opcao = int(input("Digite uma opçao"))
+    print("--> Opção 1 = Cadastrar CPF")
+    print("--> Opção 2 = Consultar CPF")
+
+    print("")
+    opcao = int(input("Digite uma opçao: "))
+    print("")
+    
     if opcao == 1:
-        cpf = input("Digite seu cpf para cadastro: ")
-        if re.fullmatch(regex_cpf, cpf):
-                print("CPF válido")
-                break
-        else:
-                print("CPF Inválido, digite novamente: ")
-                print("="*30)
+                
+          cpf = input("Digite seu cpf para cadastro: ")
+          validacao = re.fullmatch(regex_cpf, cpf)
+          while validacao == None:
+            print("CPF inválido, digite novamente: ")
+            cpf = input("Digite seu cpf para cadastro: ")
+            validacao = re.fullmatch(regex_cpf, cpf)
+          else:
+            nome =input("Digite o nome para cadastro: ")
